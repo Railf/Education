0308e00f77a92726be26f01f2322904791e58530
Ralph Wayne McCracken, III <Railf@users.noreply.github.com>
2016-10-25 17:39:09 -0500

    Create Java_Garage.java

---
+++ Java/Garage/Java_Garage.java
@@ -0,0 +1,14 @@
+/* 
+    Started on:     25 Mar 2015
+    Finished on:    
+    Ralph Wayne McCracken, III
+ */ //Methods (Math class methods)
+package java_garage;
+
+public class Java_Garage {
+    public static void main(String[] args) {
+        Garage application = new Garage();
+        application.startCharging();
+    }
+    
+}
