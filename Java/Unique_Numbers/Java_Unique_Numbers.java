241773bf8181f262b433be785e4291ae80b23b77
Ralph Wayne McCracken, III <Railf@users.noreply.github.com>
2016-10-25 17:43:41 -0500

    Create Java_Unique_Numbers.java

---
+++ Java/Unique_Numbers/Java_Unique_Numbers.java
@@ -0,0 +1,15 @@
+/*  
+    Started on:     21 April 2015
+    Finished on:    21 April 2015
+    Ralph Wayne McCracken, III
+ */ //Arrays and exception handling.
+package java_unique_numbers;
+
+public class Java_Unique_Numbers {
+    
+    public static void main(String[] args) {
+        Unique application = new Unique();
+        application.getNumbers();
+        
+    }//end main
+}//end class Java_Lab06
