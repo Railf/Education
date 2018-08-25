9a2fa9b10ef10c04685362538df9ade1e4dae5d5
Ralph Wayne McCracken, III <Railf@users.noreply.github.com>
2018-08-24 20:54:48 -0500

    Delete stats.h

--- CPP/Stats/stats.h
+++
@@ -1,8 +0,0 @@
-/*  stats.h - Function Prototypes
-    September 11, 2016
-    Ralph McCracken, III    */
-
-float Mean   (const int* array, size_t size); // calculates mean of data in array
-float Median (int* array, size_t size);       // calculates median of data in array
-void  Swap   (int& x, int& y);                // interchanges values of x and y
-void  Sort   (int* array, size_t size);       // sorts the data in array
